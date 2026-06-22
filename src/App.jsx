3afd72a9ecc56b91f8dc4d06e77b8bcad6f00920
Abhinav Anand <abhinavanand072@gmail.com>
2026-06-22 15:50:43 +0530

    Fetching data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import StartPage from './components/StartPage/StartPage.jsx'
 import QuizPage from './components/QuizPage/QuizPage.jsx'
 import './App.css'
 
 function App() {
   const [currentPage, SetCurrentPage] = useState('Start')
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('https://opentdb.com/api.php?amount=5')
+      const data = await res.json()
+      const options = data.results.map(result => {
+        const randomIndex = Math.floor(Math.random() * (result.incorrect_answers.length + 1))
+        return {
+          question: result.question,
+          options: [...result.incorrect_answers].toSpliced(randomIndex, 0, result.correct_answer),
+          answer: result.correct_answer
+        }
+      })
+      console.log(options)
+    } 
+
+    fetchData()
+  }, [])
 
   switch(currentPage) {
     case 'Start':
